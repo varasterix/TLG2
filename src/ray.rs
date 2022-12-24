52b7dc0ea3c795992e6fccf05774e73f704c1988
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2022-12-24 19:29:24 +0100

    Add preliminary implementation of ray objects

---
+++ src/ray.rs
@@ -0,0 +1,32 @@
+use crate::vec3;
+use std::fmt;
+use vec3::Vec3;
+
+pub struct Ray {
+    origin: Vec3,
+    direction: Vec3,
+}
+
+impl fmt::Display for Ray {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.origin, self.direction)
+    }
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn origin(&self) -> &Vec3 {
+        &self.origin
+    }
+
+    pub fn direction(&self) -> &Vec3 {
+        &self.direction
+    }
+
+    pub fn at(&self, other: &f32) -> Vec3 {
+        &self.origin + &(other * &self.direction)
+    }
+}
