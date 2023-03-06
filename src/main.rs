7d73609ccb3517e4e96c959d5dd3222205b5028b
Thomas Schillaci <thomas.schillaci@gmail.com>
2023-03-06 16:07:45 +0100

    Separated pixel rendering from raytracing rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use image;
 mod color;
 mod ray;
 mod vec3;
@@ -9,7 +10,7 @@ fn ray_color(r: &ray::Ray) -> Vec3 {
     return &((1.0 - t) * &Vec3::new(1.0, 1.0, 1.0)) + &(t * &Vec3::new(0.5, 0.7, 1.0));
 }
 
-fn main() {
+fn ray_based() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width: u32 = 400;
@@ -44,4 +45,34 @@ fn main() {
     }
 
     eprintln!("Done.")
+}
+
+fn pixel_based() {
+    const WIDTH: u32 = 256;
+    const HEIGHT: u32 = 256;
+    const PATH: &str = "out/out.png";
+
+    let mut buffer = vec![];
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let r = (x * 255 / WIDTH) as u8;
+            let g = (y * 255 / HEIGHT) as u8;
+            let b = (WIDTH - x) as f32 / WIDTH as f32 * (HEIGHT - y) as f32 / HEIGHT as f32;
+            let b = (b * 255.) as u8;
+            buffer.extend([r, g, b]);
+        }
+    }
+
+    image::save_buffer(
+        PATH,
+        &buffer,
+        WIDTH,
+        HEIGHT,
+        image::ColorType::Rgb8,
+    ).unwrap();
+}
+
+fn main() {
+    pixel_based();
+    // ray_based();
 }
