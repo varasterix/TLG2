997054e9ee2b1b4e88ff78e9a924e09f5812d299
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2023-03-22 00:22:53 +0100

    Improve hit property and sphere object

--- src/hittable.rs
+++ src/hittable.rs
@@ -4,10 +4,24 @@ pub struct HitRecord {
     pub p: Vec3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
 }
 
 pub trait Hittable {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
         false
+    }
+
+    fn set_face_normal(r: &Ray, outward_normal: &Vec3) {}
+}
+
+impl Hittable for HitRecord {
+    fn set_face_normal(r: &Ray, outward_normal: &Vec3) {
+        let front_face = Vec3::dot(r.direction(), outward_normal) < 0.0;
+        let neg_outward_normal = -1.0 * outward_normal; // HACK: I know, this is ugly
+        let normal = match front_face {
+            true => outward_normal,
+            false => &neg_outward_normal,
+        };
     }
 }
