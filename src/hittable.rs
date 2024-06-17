58553a0b3a5981836bbc863c6f8ebd4418feae27
Aymeric Varasse <43675676+varasterix@users.noreply.github.com>
2024-06-17 11:55:50 +0200

    Remove ugly hack

--- src/hittable.rs
+++ src/hittable.rs
@@ -18,10 +18,9 @@ pub trait Hittable {
 impl Hittable for HitRecord {
     fn set_face_normal(r: &Ray, outward_normal: &Vec3) {
         let front_face = Vec3::dot(r.direction(), outward_normal) < 0.0;
-        let neg_outward_normal = -1.0 * outward_normal; // HACK: I know, this is ugly
         let normal = match front_face {
             true => outward_normal,
-            false => &neg_outward_normal,
+            false => &(-1.0 * outward_normal),
         };
     }
 }
